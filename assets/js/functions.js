81424f5b6eee9f97ea57efe9bb0b19b65980b3ca
Clinton Halpin <clintonhalpin@Clinton-Halpins-MacBook-Pro.local>
2013-04-16 21:29:46 -0400

    Pass Value to Functionn

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,73 +1,110 @@
-(function($) {
+// (function($) {
 
-	var hypeM = {
-		init: function( config ) {
-			this.url = 'http://hypem.com/playlist/loved/clintonhalpin/json/1/data.json';
-			this.template = config.template;
-			this.container = config.container;
-			this.fetch();			
-		},
+// 	var hypeM = {
+// 		init: function( config ) {
+// 			this.url = 'http://hypem.com/playlist/loved/clintonhalpin/json/1/data.json';
+// 			this.template = config.template;
+// 			this.container = config.container;
+// 			this.fetch();			
+// 		},
 
-		attachTemplate: function() {
-			var template = Handlebars.compile( this.template );
-			this.container.append( template( this.hypeData ) );
-		},
+// 		attachTemplate: function() {
+// 			var template = Handlebars.compile( this.template );
+// 			this.container.append( template( this.hypeData ) );
+// 		},
 
 		
-		// bindEvents: function() {
-		// 	this.searchInput.on( 'keyup', this.search );
-		// },
+// 		// bindEvents: function() {
+// 		// 	this.searchInput.on( 'keyup', this.search );
+// 		// },
 
-		// search: function() {
-		// 	var self = Twitter,
-		// 		input = this;
+// 		// search: function() {
+// 		// 	var self = Twitter,
+// 		// 		input = this;
 
-		// 	clearTimeout( self.timer );
+// 		// 	clearTimeout( self.timer );
 
-		// 	self.timer = ( input.value.length >= 3 ) && setTimeout(function() {
-		// 		self.query = input.value;
-		// 		$.publish( 'twitter/query' );
-		// 	}, 400);
-		// },
+// 		// 	self.timer = ( input.value.length >= 3 ) && setTimeout(function() {
+// 		// 		self.query = input.value;
+// 		// 		$.publish( 'twitter/query' );
+// 		// 	}, 400);
+// 		// },
 
 
-		fetch: function() {
-			var self = this;
+// 		fetch: function() {
+// 			var self = this;
 
-			$.getJSON( this.url, function( data ) {
-				self.hypeData = $.map( data, function( hypeData ) {
-					return {
-						mediaid: hypeData.mediaid,  
-						artist: hypeData.artist,  
-						title: hypeData.title,  
-						dateposted: hypeData.dateposted,  
-						siteid: hypeData.siteid, 
-						sitename: hypeData.sitename, 
-						posturl: hypeData.posturl, 
-						postid: hypeData.postid, 
-						loved_count: hypeData.loved_count, 
-						posted_count: hypeData.posted_count, 
-						thumb_url: hypeData.thumb_url, 
-						thumb_url_medium: hypeData.thumb_url_medium, 
-						thumb_url_large: hypeData.thumb_url_large, 
-						thumb_url_artist: hypeData.thumb_url_artist, 
-						time: hypeData.time, 
-						description: hypeData.description,
-						dateloved: hypeData.dateloved, 
-						itunes_link: hypeData.itunes_link
-					};
-				});
+// 			$.getJSON( this.url, function( data ) {
+// 				self.hypeData = $.map( data, function( hypeData ) {
+// 					return {
+// 						mediaid: hypeData.mediaid,  
+// 						artist: hypeData.artist,  
+// 						title: hypeData.title,  
+// 						dateposted: hypeData.dateposted,  
+// 						siteid: hypeData.siteid, 
+// 						sitename: hypeData.sitename, 
+// 						posturl: hypeData.posturl, 
+// 						postid: hypeData.postid, 
+// 						loved_count: hypeData.loved_count, 
+// 						posted_count: hypeData.posted_count, 
+// 						thumb_url: hypeData.thumb_url, 
+// 						thumb_url_medium: hypeData.thumb_url_medium, 
+// 						thumb_url_large: hypeData.thumb_url_large, 
+// 						thumb_url_artist: hypeData.thumb_url_artist, 
+// 						time: hypeData.time, 
+// 						description: hypeData.description,
+// 						dateloved: hypeData.dateloved, 
+// 						itunes_link: hypeData.itunes_link
+// 					};
+// 				});
 
-				self.attachTemplate(); 
-			});
-		},
-	};
+// 				self.attachTemplate(); 
+// 			});
+// 		},
+// 	};
 
-	hypeM.init({
-		template: $('#favorites-template').html(),
-		container: $('.hypeData'),
-		username: 'clintonhalpin'
+// 	hypeM.init({
+// 		template: $('#favorites-template').html(),
+// 		container: $('.hypeData'),
+// 		username: 'clintonhalpin'
+// 	});
+
+// })(jQuery);
+
+
+// $('.form-search').submit(function() {
+	
+// 	var v = $('.form-search').val();   
+
+// 	console.log(v);
+
+
+
+// 	return false;
+
+
+
+// });
+
+(function($) {
+
+
+
+	$(".search-hypeM").on('keyup', function(e) {
+		    if (e.which == 13) {
+				
+				var value = $(this).val();
+				
+				getPizza(value);
+
+		        e.preventDefault();
+		    }
 	});
+
+	function getPizza(value){
+		console.log(value);
+	}
+
 
 })(jQuery);
 
