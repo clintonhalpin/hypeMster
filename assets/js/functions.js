0c9db25e955a8083016f6ab1c7c034b8900dd2b8
Clinton Halpin <clintonhalpin@Clinton-Halpins-MacBook-Pro.local>
2013-04-17 23:04:32 -0400

    Error Messaging on the dumb

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,7 @@
 (function($) {
 
 	var hypeM = {
+
 		init: function( config ) {
 			this.url = 'http://hypem.com/playlist/loved/' + config.username + '/json/1/data.json';
 			this.template = config.template;
@@ -9,56 +10,69 @@
 		},
 
 		attachTemplate: function(hypeData) {
-			
-
 			var template = Handlebars.compile( this.template );
 
-			this.container.empty();
+			// Empty the Container
 
+			this.container.empty();
 			
+			// Compile the Container
+
 			this.container.append( template( this.hypeData ) );
 		},
 
-
-
-		
 		bindEvents: function() {
 			var self = hypeM;
 
 			$(".search-hypeM").keydown(function(e) {
 				if (e.which == 13) {
 					var value = $(this).val();
-
 					self.fetch(value);
-
 					return false;
 				}
 			});
-
 		},
-
 		
 		fetch: function(value) {
-
 			var self = this,
 				v = encodeURIComponent(value.trim()),
 				url = 'http://hypem.com/playlist/loved/' + v + '/json/1/data.json';
 
 			$.getJSON( url, function( data ) {
+
+				
+
 				self.hypeData = $.map( data, function( hypeData ) {
-					return {
-						version: hypeData.version, mediaid: hypeData.mediaid,  artist: hypeData.artist,  title: hypeData.title,  dateposted: hypeData.dateposted,  siteid: hypeData.siteid, sitename: hypeData.sitename, posturl: hypeData.posturl, postid: hypeData.postid, loved_count: hypeData.loved_count, posted_count: hypeData.posted_count, thumb_url: hypeData.thumb_url, thumb_url_medium: hypeData.thumb_url_medium, thumb_url_large: hypeData.thumb_url_large, thumb_url_artist: hypeData.thumb_url_artist, time: hypeData.time, description: hypeData.description,dateloved: hypeData.dateloved, itunes_link: hypeData.itunes_link
-					};
+					
+						if (version = 1.1) {
+							return {
+								artist: 'No Results for ' + value
+							};
+						} else {
+							return {
+								version: hypeData.version, mediaid: hypeData.mediaid,  artist: hypeData.artist,  title: hypeData.title,  dateposted: hypeData.dateposted,  siteid: hypeData.siteid, sitename: hypeData.sitename, posturl: hypeData.posturl, postid: hypeData.postid, loved_count: hypeData.loved_count, posted_count: hypeData.posted_count, thumb_url: hypeData.thumb_url, thumb_url_medium: hypeData.thumb_url_medium, thumb_url_large: hypeData.thumb_url_large, thumb_url_artist: hypeData.thumb_url_artist, time: hypeData.time, description: hypeData.description,dateloved: hypeData.dateloved, itunes_link: hypeData.itunes_link
+							};
+						}
 			});
 				self.attachTemplate(); 
-			});			
+			});	
+
+
+			// $.getJSON( url, function(data) {
+			// 	console.log( "success" );
+			// })
+			// .done(function() { 
+			// 	console.log( "second success" );
+			// })
+			// .fail(function() { console.log( "error" ); })
+					
 		}
 	};
 
 	hypeM.init({
 		template: $('#favorites-template').html(),
 		container: $('.hypeData'),
-		username: 'anthony a'
+		username: 'clintonhalpin'
 	});
 
 })(jQuery);
