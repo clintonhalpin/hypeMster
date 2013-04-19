70f99204b9d1276c0d24a6fe94a79e09ddaf0846
Clinton Halpin <clintonhalpin@gmail.com>
2013-04-18 21:28:42 -0400

    Cast a dark shadow on .DS_Store and probably something else in the future

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -36,19 +36,17 @@
 		fetch: function(value) {
 			var self = this,
 				v = encodeURIComponent(value.trim()),
-				url = 'http://hypem.com/playlist/loved/' + v + '/json/1/data.json';
+				urlUsername = 'http://hypem.com/playlist/loved/' + v + '/json/1/data.json',
+				urlSearch = 'http://hypem.com/playlist/search/' + v + '/json/1/data.json';
 
-			$.getJSON( url, function( data ) {
 
-				
+
+			$.getJSON( urlSearch, function( data ) {
 
 				self.hypeData = $.map( data, function( hypeData ) {
-					
-						
-							return {
-								version: hypeData.version, mediaid: hypeData.mediaid,  artist: hypeData.artist,  title: hypeData.title,  dateposted: hypeData.dateposted,  siteid: hypeData.siteid, sitename: hypeData.sitename, posturl: hypeData.posturl, postid: hypeData.postid, loved_count: hypeData.loved_count, posted_count: hypeData.posted_count, thumb_url: hypeData.thumb_url, thumb_url_medium: hypeData.thumb_url_medium, thumb_url_large: hypeData.thumb_url_large, thumb_url_artist: hypeData.thumb_url_artist, time: hypeData.time, description: hypeData.description,dateloved: hypeData.dateloved, itunes_link: hypeData.itunes_link
-							};
-						
+					return {
+						version: hypeData.version, mediaid: hypeData.mediaid,  artist: hypeData.artist,  title: hypeData.title,  dateposted: hypeData.dateposted,  siteid: hypeData.siteid, sitename: hypeData.sitename, posturl: hypeData.posturl, postid: hypeData.postid, loved_count: hypeData.loved_count, posted_count: hypeData.posted_count, thumb_url: hypeData.thumb_url, thumb_url_medium: hypeData.thumb_url_medium, thumb_url_large: hypeData.thumb_url_large, thumb_url_artist: hypeData.thumb_url_artist, time: hypeData.time, description: hypeData.description,dateloved: hypeData.dateloved, itunes_link: hypeData.itunes_link
+					};
 			});
 				self.attachTemplate(); 
 			});	
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+	include 'inc/header.php';
+	include 'inc/tmpls/page-header.php';
+?>
+
+<!-- Page Contents -->
+	
+	<div class="hypeSearch">
+		<form class="form-search">
+			<input type="text" placeholder="Search the Hype Machine" class="search-hypeM">
+		</form>
+	</div>
+	
+	<div class="hypeData"></div>
+
+<!-- / Page Contents -->		
+
+<?php 
+	include 'inc/footer.php';
+?>
