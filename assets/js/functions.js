aff671bf79da246a242c78930dfb38f50dc801d6
Clinton Halpin <clintonhalpin@gmail.com>
2013-04-20 12:10:01 -0400

    Added Local JSON response

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -38,11 +38,13 @@
 		
 		fetch: function(value) {
 			var self = this,
-				v = encodeURIComponent(value.trim()),
-				urlUsername = 'http://hypem.com/playlist/loved/' + v + '/json/1/data.json',
-				urlSearch = 'http://hypem.com/playlist/search/' + v + '/json/1/data.json';
+				// v = encodeURIComponent(value.trim()),
+				// urlUsername = 'http://hypem.com/playlist/loved/' + v + '/json/1/data.json',
+				// urlSearch = 'http://hypem.com/playlist/search/' + v + '/json/1/data.json';
 
-			$.getJSON( urlSearch, function( data ) {
+				urlSearchBaked = "http://localhost/develop/hypem/v1/sampledata.json";
+
+			$.getJSON( urlSearchBaked, function( data ) {
 				self.hypeData = $.map( data, function( hypeData ) {
 					return {
 						version: hypeData.version, mediaid: hypeData.mediaid,  artist: hypeData.artist,  title: hypeData.title,  dateposted: hypeData.dateposted,  siteid: hypeData.siteid, sitename: hypeData.sitename, posturl: hypeData.posturl, postid: hypeData.postid, loved_count: hypeData.loved_count, posted_count: hypeData.posted_count, thumb_url: hypeData.thumb_url, thumb_url_medium: hypeData.thumb_url_medium, thumb_url_large: hypeData.thumb_url_large, thumb_url_artist: hypeData.thumb_url_artist, time: hypeData.time, description: hypeData.description,dateloved: hypeData.dateloved, itunes_link: hypeData.itunes_link
