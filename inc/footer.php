a7ed9e52e898cd12aee803d0b689a497458bd3b5
clintonhalpin <clintonhalpin@gmail.com>
2013-06-08 19:19:36 -0400

    Clean up of Files

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,11 @@
 </div><!-- / Wrapper -->
 <!-- Dependencys -->
-	<script type="text/javascript" src="assets/js/jquery-1.72.js"></script>
-	<script type="text/javascript" src="assets/js/handlebars.js"></script>
+	<script type="text/javascript" src="assets/js/vendor/jquery-1.72.js"></script>
+	<script type="text/javascript" src="assets/js/vendor/handlebars.js"></script>
 <!-- App Scripts -->
 	<?php include 'tmpls/view-handlebars/searchResults.php'; ?>
 	
-	<script type="text/javascript" src="assets/js/functions.js"></script>
+	<script type="text/javascript" src="assets/js/app.js"></script>
 </body>
 </html>
 
