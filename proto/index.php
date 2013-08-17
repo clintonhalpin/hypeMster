171affd09149040e5f1c6e0eb2b54af9220d6a39
clintonhalpin <clintonhalpin@gmail.com>
2013-08-17 00:13:31 -0400

    Add proto dir for new UI

---
+++ proto/index.php
@@ -0,0 +1,14 @@
+<?php 
+	include '../inc/header.php';
+	include '../inc/tmpls/page-header.php';
+?>
+
+<!-- Page Contents -->
+	
+	<div class="hypeResults"><h1>Search the HypeM!</h1></div>
+
+<!-- / Page Contents -->		
+
+<?php 
+	include '../inc/footer.php';
+?>
